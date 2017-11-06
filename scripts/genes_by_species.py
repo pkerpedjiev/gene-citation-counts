44349647aed3fdd4df9880c0cff8be435b153766
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-11-05 17:45:35 -0700

    Updated genes_to_species and added a line referencing the file output by clodius as part of the gene annotations creation

--- scripts/genes_by_species.py
+++ scripts/genes_by_species.py
@@ -1,4 +1,5 @@
 import argparse
+import collections as col
 import sys
 
 def main():
@@ -8,11 +9,33 @@ def main():
     Count how many citations there are for each species.
 """)
 
-    #parser.add_argument('argument', nargs=1)
+    parser.add_argument('filename')
     #parser.add_argument('-o', '--options', default='yo',
     #					 help="Some option", type='str')
     #parser.add_argument('-u', '--useless', action='store_true', 
     #					 help='Another useless option')
-    parser.add_argument('filepath')
 
     args = parser.parse_args()
+
+    if args.filename == '-':
+        f = sys.stdin
+    else:
+        f = open(args.filename)
+
+    citations_by_taxid = col.defaultdict(set)
+
+    for line in f:
+        # usually the top line will contain header information
+        if line[0] == '#':
+            continue
+        parts = line.strip().split()
+        taxid = parts[0]
+        citation = parts[2]
+
+        citations_by_taxid[taxid].add(citation)
+
+    for key in citations_by_taxid:
+        print("{}\t{}".format(key, len(citations_by_taxid[key])))
+
+if __name__ == "__main__":
+    main()
